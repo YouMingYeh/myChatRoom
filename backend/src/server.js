02475925692af5888864967cf3f81004a40a118b
yehyouming <b10705052@ntu.edu.tw>
2022-12-26 18:56:08 +0800

    .

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,25 @@ import WebSocket from "ws";
 import mongo from "./mongo";
 import wsConnect from "./wsConnect";
 import { v4 as uuid } from 'uuid';
+import cors from 'cors';
+import path from 'path';
 mongo.connect();
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
+
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const db = mongoose.connection;
